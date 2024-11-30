eccff9a52e1513f7cb167c9f509bc6803f854c0b
_cwoo2 <785808933@qq.com>
2024-11-30 15:14:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from weibo import fetch_weibo
+from hot_36kr import fetch_36kr
+
+def test_main():
+    print('test_main')
 
 def main():
     fetch_weibo()
+    fetch_36kr()
 
 if __name__ == '__main__':
     main()
