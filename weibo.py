8e765dfd5fe18164ce6c6bf34db09b76caacddac
_cwoo2 <785808933@qq.com>
2024-11-30 14:40:43 +0800

    Update weibo.py

--- weibo.py
+++ weibo.py
@@ -13,7 +13,8 @@ def fetch_weibo():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'CLIENT-IP': ip,
-        'X-FORWARDED-FOR': ip
+        'X-FORWARDED-FOR': ip,
+        'Referer': 'https://www.baidu.com',
     }
 
     # 热搜地址
