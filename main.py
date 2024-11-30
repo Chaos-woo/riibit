58578b10f6ce0fbe3ee9ca3ad6448dd041c291b9
_cwoo2 <785808933@qq.com>
2024-11-30 15:37:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from weibo import fetch_weibo
 from hot_36kr import fetch_36kr
+from zhihu import fetch_zhihu
+from zhihu_daily import fetch_zhihu_daily
 
 def test_main():
     print('test_main')
 
+
 def main():
     fetch_weibo()
     fetch_36kr()
+    fetch_zhihu()
+    fetch_zhihu_daily()
 
 if __name__ == '__main__':
     main()
